a97ce26db350736eaf60cdf7947f38570edd8595
pafo <pafo@PLLL0271.gft.com>
2016-09-22 13:33:12 +0200

    Refactor

--- src/main/java/com/filocha/Application.java
+++ src/main/java/com/filocha/Application.java
@@ -1,23 +1,16 @@
 package com.filocha;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
 public class Application {
-
-	public static final List<String> dirs = new ArrayList<>();
 
 	public static void main(String[] args) {
 
-		FileBranchAdapter root = new FileBranchAdapter(new File("c:/Temp/"));
-
-		Iterable<Leaf> items = TreeUtil.convert(root);
-		Iterator<Leaf> iterator = items.iterator();
-
-		StreamUtils.asStream(iterator)
-				.forEach(it -> System.out.println(it.getName()));
+		// FileBranchAdapter root = new FileBranchAdapter(new File("c:/Temp/"));
+		//
+		// Iterable<Leaf> items = TreeUtil.convert(root);
+		// Iterator<Leaf> iterator = items.iterator();
+		//
+		// StreamUtils.asStream(iterator)
+		// .forEach(it -> System.out.println(it.getName()));
 
 	}
 
