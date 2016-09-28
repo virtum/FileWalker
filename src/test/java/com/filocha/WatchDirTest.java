85263acbd6f68d43ad8c71c8113c3edad528a12c
pafo <pafo@PLLL0271.gft.com>
2016-09-28 12:33:18 +0200

    Another refactor of previous code

--- src/test/java/com/filocha/WatchDirTest.java
+++ src/test/java/com/filocha/WatchDirTest.java
@@ -4,10 +4,12 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.UUID;
 
+import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.junit.Test;
 
 import rx.subjects.ReplaySubject;
@@ -15,13 +17,16 @@ import rx.subjects.ReplaySubject;
 public class WatchDirTest {
 
 	@Test
-	public void shouldReceiveChangesInFolders() {
-		String home = System.getProperty("user.home") + "\\Temp\\";
+	public void shouldReceiveChangesInFolders() throws IOException {
+		String home = System.getProperty("user.home");
+		File temp = new File(home + "\\Temp");
+		temp.mkdir();
+
 		String randomName = UUID.randomUUID().toString().substring(28);
-		String root = home + randomName + "\\";
+		String root = temp + "\\" + randomName + "\\";
 
 		boolean recursive = true;
-		Path dir = Paths.get(home);
+		Path dir = Paths.get(temp.getAbsolutePath());
 		WatchDir watcher = new WatchDir(dir, recursive);
 
 		ReplaySubject<Path> results = ReplaySubject.create();
@@ -35,6 +40,6 @@ public class WatchDirTest {
 		assertThat(file.exists(), equalTo(true));
 		assertThat(result.toFile().getName(), equalTo(file.getName()));
 
-		file.delete();
+		FileUtils.deleteDirectory(temp);
 	}
 }
