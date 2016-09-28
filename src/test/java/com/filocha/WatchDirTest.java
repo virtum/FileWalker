3f6ade34fccd24d1f7a1bf5a6375349d5436f6ee
pafo <pafo@PLLL0271.gft.com>
2016-09-28 12:25:28 +0200

    Changed test implementation to use local variable

--- src/test/java/com/filocha/WatchDirTest.java
+++ src/test/java/com/filocha/WatchDirTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertThat;
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.UUID;
 
 import org.junit.Test;
 
@@ -15,14 +16,18 @@ public class WatchDirTest {
 
 	@Test
 	public void shouldReceiveChangesInFolders() {
+		String home = System.getProperty("user.home") + "\\Temp\\";
+		String randomName = UUID.randomUUID().toString().substring(28);
+		String root = home + randomName + "\\";
+
 		boolean recursive = true;
-		Path dir = Paths.get("C:\\Temp\\");
+		Path dir = Paths.get(home);
 		WatchDir watcher = new WatchDir(dir, recursive);
 
 		ReplaySubject<Path> results = ReplaySubject.create();
 		watcher.createObservable().subscribe(results);
 
-		File file = new File("C:\\Temp\\xxx");
+		File file = new File(root);
 		file.mkdirs();
 
 		Path result = results.toBlocking().first();
